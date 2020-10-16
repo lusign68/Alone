dbf2ec270195b3909e331a4da44fd6c16c127942
lusign68 <37559540+lusign68@users.noreply.github.com>
2020-10-16 18:15:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,9 +6,44 @@ var canvas = document.getElementsByTagName("canvas")[0],
 		window.mozRequestAnimationFrame ||
 		(cb => window.setTimeout(cb, 1000 / 30));
 
+var data = document.getElementById('data');
+var text;
+
+//GUI
+var sampleText = function() {
+  this.message = "dat.guiのサンプル";
+  this.color = "#ff0000";
+  this.fontSize = 24;
+  this.border = false;
+  this.fontFamily = "sans-serif";
+};
+
+//GUI
 window.onload = function() {
+  text = new sampleText();
+  setValue();
   var gui = new dat.GUI();
   gui.add(text, 'message').onChange(setValue);
+  gui.addColor(text, 'color').onChange(setValue);
+  gui.add(text, 'fontSize', 6, 48).onChange(setValue);
+  gui.add(text, 'border').onChange(setValue);
+  gui.add(text, 'fontFamily',["sans-serif", "serif", "cursive", "ＭＳ 明朝", "monospace"]).onChange(setValue);
+};
+
+//2
+function setValue() {
+  data.innerHTML = text.message;
+  data.style.color = text.color;
+  data.style.fontSize = text.fontSize+"px";
+  data.style.fontFamily = text.fontFamily;
+  if(text.border) {
+    data.style.border = "solid 1px black";
+    data.style.padding = "10px";
+  }
+  else {
+    data.style.border = "none";
+    data.style.padding = "0px";
+  }
 }
 
 function sign() {
